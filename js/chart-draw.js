9b9c59115a4c473ec701dd717b5813761387c66b
Hiroyuki Shima <shima_hiroyuki@applibot.co.jp>
2020-04-05 18:50:21 +0900

    remove timeline chart

--- js/chart-draw.js
+++ js/chart-draw.js
@@ -1,5 +1,4 @@
 var countryRankingChart = null;
-var timeLineChart = null;
 
 function initCountryRankingChart(countryInfoList) {
   var options = {
@@ -108,78 +107,4 @@ function reDrawCountryRankingChart(countryInfoList){
     newOption.series[1].data.push(countryInfo.deaths.total);
   });
   countryRankingChart.updateOptions(newOption)
-
-}
-
-function initTimeLineChart(countryInfoList) {
-  var options = {
-    series: [
-      {
-        name: "total",
-        data: []
-      },
-      {
-        name: "deaths",
-
-        data: []
-      },
-    ],
-    chart: {
-      type: "bar",
-      width: "100%",
-      height: countryInfoList.length * 80,
-      background: '#F2F2F2'
-    },
-    colors: ["#3EC6E0", "#FF4560"],
-    plotOptions: {
-      bar: {
-        horizontal: true,
-        barHeight: '100%',
-        dataLabels: {
-          position: 'bottom'
-        },
-
-      },
-    },
-    stroke: {
-      width: 1,
-      colors: ['#fff']
-    },
-    dataLabels: {
-      offsetX: 10,
-      dropShadow: {
-        enabled: true,
-         opacity: 0.6
-      },
-      style: {
-        colors: ['#fff'],
-        fontSize: '20px',
-      },
-    },
-    xaxis: {
-      categories: [],
-      labels:{
-        style: {
-           fontSize: '20px',
-       }
-      }
-    },
-    yaxis: {
-      labels:{
-        style: {
-           fontSize: '20px',
-       }
-      }
-    }
-}
-
-  timeLineChart = new ApexCharts(document.querySelector("#timeLineChart"), options);
-
-  countryInfoList.forEach(countryInfo => {
-    options.xaxis.categories.push(countryInfo.country);
-    options.series[0].data.push(countryInfo.cases.total);
-    options.series[1].data.push(countryInfo.deaths.total);
-  });
-
-  timeLineChart.render();
 }
--- js/covid-overview.js
+++ js/covid-overview.js
@@ -9,27 +9,31 @@ var app = new Vue({
     rankingTo: 10
   },
   mounted () {
-    axios
-      .get('https://covid-193.p.rapidapi.com/statistics',  {headers: {
-        "x-rapidapi-host": "covid-193.p.rapidapi.com",
-        "x-rapidapi-key": "6351089e97mshd70d80573520055p1d9190jsn4f1938e461c0"
-      }})
-      .then(response => {
-        response.data.response.forEach(dto => {
-            var tmpList = [];
-            if(dto.country == 'World' || dto.country == 'All') {
-                this.summary = dto;
-            } else if (dto.country == 'Japan') {
-                this.japanInfo = dto;
-            } else {
-              this.countryInfoList.push(dto);
-            }
-        })
-        this.countryInfoList.sort((a, b) => (b.cases.total > a.cases.total) ? 1 : -1)
-        initCountryRankingChart(this.countryInfoList.slice(this.rankingFrom - 1, this.rankingTo));
-      })
+    this.initCountryInfo();
+
   },
   methods:{
+      initCountryInfo: function() {
+        axios
+        .get('https://covid-193.p.rapidapi.com/statistics',  {headers: {
+          "x-rapidapi-host": "covid-193.p.rapidapi.com",
+          "x-rapidapi-key": "6351089e97mshd70d80573520055p1d9190jsn4f1938e461c0"
+        }})
+        .then(response => {
+          response.data.response.forEach(dto => {
+              var tmpList = [];
+              if(dto.country == 'World' || dto.country == 'All') {
+                  this.summary = dto;
+              } else if (dto.country == 'Japan') {
+                  this.japanInfo = dto;
+              } else {
+                this.countryInfoList.push(dto);
+              }
+          })
+          this.countryInfoList.sort((a, b) => (b.cases.total > a.cases.total) ? 1 : -1)
+          initCountryRankingChart(this.countryInfoList.slice(this.rankingFrom - 1, this.rankingTo));
+        })
+      },
       drawChartWithPrevious: function() {
         if(this.rankingFrom - COUNTRIES_PER_PAGE < 0) {
           this.rankingTo = COUNTRIES_PER_PAGE;
